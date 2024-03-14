31e25086d114345951a8ffab703db35f428aec63
JerssonQuintero <jersson.9.5@gmail.com>
2024-03-13 22:54:51 -0500

    Contador sencillo

---
+++ 02-bases/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { CounterComponent } from './counter/counter.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CounterComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
