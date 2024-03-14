f33f849176d2143dbaff511c1bc02f8467bc3ffb
Jersson_quintero <jersson.9.5@gmail.com>
2024-03-14 12:07:38 -0500

    Creando components por cli

--- 02-bases/src/app/app.component.ts
+++ 02-bases/src/app/app.component.ts
@@ -7,12 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public title:string = 'Hola mundo';
-  public counter:number =10;
 
-  increaseBy( value:number ):void{
-      this.counter +=value;
-  }
-  reset():void{
-    this.counter=10;
-  }
+
 }
--- 02-bases/src/app/counter/counter.component.ts
+++ 02-bases/src/app/counter/counter.component.ts
@@ -1,10 +1,26 @@
-import { Component } from "@angular/core";
-
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
-  template:'<h1>hola Counter</h1>',
+  template: `
+  <h2>Primer Counter con CounterComponent.ts </h2>
+
+<h3>Counter:{{counter}}</h3>
+<button (click)="increaseBy(1)" >+1</button>
+<button (click)="reset()" >Reset</button>
+<button (click)="increaseBy(-1)" >-1</button>
+  `
 })
-export class CounterComponent{
+
+export class CounterComponent {
+  public counter:number=10;
+  constructor() { }
+
+  increaseBy (value:number):void{
+    this.counter+=value;
+  }
+  reset():void{
+    this.counter=10;
+  }
 
 }
