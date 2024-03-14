92acb73a2394f9d44fd4e9b30d153f4c83e9f2a2
Jersson_quintero <jersson.9.5@gmail.com>
2024-03-14 17:11:15 -0500

    ngIf para Heroes

--- 02-bases/src/app/heroes/hero/hero.component.ts
+++ 02-bases/src/app/heroes/hero/hero.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrl: './hero.component.css'
 })
 export class HeroComponent {
+  public name : string = 'ironman';
+  public age: number=35;
+
+  get capitalizedName():string{
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription():string{
+
+    return `${this.name}-${this.age}`
+  }
+
+  changeHero():void{
+    // throw 'Metodo no implementado'
+     this.name="Spiderman";
+  }
+  changeAge():void{
+    this.age=22;
+  }
+  resetForm():void{
+    this.name='ironman';
+    this.age=45;
+  }
 
 }
